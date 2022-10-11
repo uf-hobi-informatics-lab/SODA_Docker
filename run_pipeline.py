f80cff6d3bc50f938b51e4087309344dee6d4c5a
James Huang <jameshuang@ahc-hobi-gpu01.ahc.ufl.edu>
2022-10-11 07:50:20 -0400

    update pipeline main script

--- run_pipeline.py
+++ run_pipeline.py
@@ -3,7 +3,7 @@ from NLPreprocessing.annotation2BIO import generate_BIO, pre_processing, BIOdata
 from NLPreprocessing.text_process.sentence_tokenization import SentenceBoundaryDetection
 # from ClinicalTransformerNER.src.run_transformer_batch_prediction import multiprocessing_wrapper, argparser, main
 from ClinicalTransformerNER.src.transformer_ner.transfomer_log import TransformerNERLogger
-from ClinicalTransformerNER.src.common_utils.output_format_converter import main as format_converter
+from ClinicalTransformerNER.src.common_utils.output_format_converter import bio2output as format_converter
 from ClinicalTransformerNER.src.run_transformer_batch_prediction import main as run_ner
 MIMICIII_PATTERN = "\[\*\*|\*\*\]"
 import torch.multiprocessing as mp
@@ -17,14 +17,12 @@ from argparse import Namespace
 
 # , note_mod_idx, root_dir=None, gpu_node=0, result='output'
 
-def get_subdirs(p):
-    sub_ps = [(p / x) for x in os.listdir(p)]
-    return sum([get_subdirs(x) for x in sub_ps], []) if len(sub_ps) else [p]
+OUTPUT_DIR = ['raw_text', 'encoded_text', 'bio_init', 'brat', 'tsv', 'brat_neg', 'brat_re', 'meta', 'brat_postproc', 'csv_output', 'logs']
 
 # TODO: notes in subdirectories
 class BatchProcessor(object):
 
-    def __init__(self, root_dir=None, raw_data_dir=None, device=None, gpu_nodes=None, output=None, batch_sz=None, 
+    def __init__(self, root_dir=None, raw_data_dir=None, device=None, gpu_nodes=None, result=None, batch_sz=None, 
                  ner_model={}, relation_model={}, negation_model={}, sent_tokenizer={}, dependency_tree=[]):
   
         self.device                     = device
@@ -37,7 +35,7 @@ class BatchProcessor(object):
         self.sent_tokenizer_params      = sent_tokenizer
         self.gpu_idx                    = gpu_nodes[0]
         self.n_gpu_nodes                = gpu_nodes[1]
-        self.output                     = output 
+        self.result                     = result 
         self.dependency_tree            = defaultdict(list)
         for dependency in dependency_tree:
             self.dependency_tree[dependency[0]].append(dependency[1])
@@ -46,8 +44,8 @@ class BatchProcessor(object):
 
         self.encoded_text               = {}
         self.bio_init                   = {}
-        self.brat                       = {}
         self.sent_bounds                = {}
+        self.brat                       = {}
         self.entities                   = {}
         self.relations                  = {}
         self.negations                  = {}
@@ -65,30 +63,27 @@ class BatchProcessor(object):
                 
         self.encoded_text       = {}
         self.bio_init           = {}
-        self.brat               = {}
         self.sent_bounds        = {}
+        self.brat               = {}
         self.entities           = {}
         self.relations          = {}
         self.negations          = {}
 
-    def read_encoded_text(ifn):
-        with open(ifn, "r") as f:
-            txt = f.read()
-        return txt
+    # @property
+    # def sent_bounds(self):
+    #     if self.bio_init:
+    #         for k, v in self.bio_init.items():
+    #             if k in self._sent_bounds:
+    #                 continue
+    #             else:
+    #                 self._sent_bounds[k] = [(sent[0][1][0], sent[-1][1][1]) for sent in v]
+    #         return self._sent_bounds
+    #     else:
+    #         return {}
 
-    def read_bio(ifn):
-        with open(ifn, "r") as f:
-            txt = f.read().strip()
-
-            if len(txt) < 1:
-                warnings.warn(f"{ifn} is an empty file.")
-                return []
-
-            sents = txt.split("\n\n")
-        return sents    
-
-    def read_brat(ifn):
-        return read_annotation_brat(ifn, include_id=True)[1]
+    def get_subdirs(self, p):
+        sub_ps = [x for x in p.iterdir() if x.is_dir() and x.stem not in OUTPUT_DIR]
+        return sum([self.get_subdirs(x) for x in sub_ps], []) if len(sub_ps) else [p]
     
     def get_batch_files(self, subdir, extension='txt'):
         batch_files = []
@@ -99,34 +94,64 @@ class BatchProcessor(object):
                 yield batch_files
                 batch_files = []
         if batch_files:
-            return batch_files
+            yield batch_files
 
-    def load_output(self, output, batch_files):
+    def load_result(self, result, batch_files):
         
         extention_dict = {'encoded_text': 'txt', 
                           'bio_init'    : 'bio', 
                           'brat'        : 'ann', 
                           'brat_re'     : 'ann'}
         
-        if output == 'raw_text':
+        if result == 'raw_text':
             return True
-        elif output in extention_dict:
+        elif result in extention_dict:
             _exist = True
             for file in batch_files:
-                output_cache = getattr(self, output)
+                output_cache = getattr(self, result)
                 if file.stem in output_cache:
                     continue
-                if (self._root_dir / output / '.'.join([file.name, extention_dict[output]])).is_file():
-                    _load_funct = getattr(self, f'read_{output}')
-                    output_cache[file.stem] = _load_funct(file)
+                _file = self._root_dir / result / '.'.join([file.stem, extention_dict[result]])
+                if _file.is_file():
+                    _load_funct = getattr(self, f'read_{result}')
+                    output_cache[file.stem] = _load_funct(_file)
                 else:
                     _exist = False
-            return _exist        
+            return _exist
         else:
             raise NotImplementedError()
 
-    def get_encoded_text(self, batch_files):
+    def read_encoded_text(self, ifn):
+        with open(ifn, "r") as f:
+            txt = f.read()
+        return txt
 
+    def read_bio_init(self, ifn):
+        
+        def word_formatting(x):
+            # if len(x) == 5:  # TODO: not sure if it capture all the corner cases
+            #     x.insert(0, '\xa0')
+            # elif len(x) != 6:
+            #     raise NotImplementedError()
+            return [x[0], (int(x[1]), int(x[2])), (int(x[3]), int(x[4])), x[5]]
+        
+        with open(ifn, "r") as f:
+            txt = f.read().strip()
+
+            if len(txt) < 1:
+                warnings.warn(f"{ifn} is an empty file.")
+                return []
+
+            sents = [[word_formatting(word.split(' ')) for word in sent.split("\n")] for sent in txt.split("\n\n")]
+            self.sent_bounds[ifn.stem] = [(sent[0][1][0], sent[-1][1][1]) for sent in sents]
+        return sents
+
+    def read_brat(self, ifn):
+        return read_annotation_brat(ifn, include_id=True)[1]
+
+    def get_encoded_text(self, batch_files):
+        (self._root_dir / 'encoded_text').mkdir(parents=True, exist_ok=True)
+        
         for file in batch_files:
             with open(file, "r", encoding='latin') as f:
                 txt = f.read()
@@ -136,90 +161,61 @@ class BatchProcessor(object):
                 f.write(txt)
 
     def get_bio_init(self, batch_files):
+        (self._root_dir / 'bio_init').mkdir(parents=True, exist_ok=True)
         
         if self.sent_tokenizer is None:
-            obj_class = globals()[self.sent_tokenizer_params.get('class')]
+            obj_class = globals()[self.sent_tokenizer_params.get('_class')]
             self.sent_tokenizer = obj_class()
 
         for file in batch_files:
+            if file.stem in self.bio_init:
+                continue
             _, sents = pre_processing(file, deid_pattern=self.sent_tokenizer_params['params']['deid_pattern'], sent_tokenizer=self.sent_tokenizer)
-            nsents, _ = generate_BIO(sents, [], file_id=file, no_overlap=False)
+            nsents, sent_bounds = generate_BIO(sents, [], file_id=file, no_overlap=False)
             self.bio_init[file.stem] = nsents
+            self.sent_bounds[file.stem] = sent_bounds
             BIOdata_to_file(self._root_dir / 'bio_init' / (file.stem + '.bio'), nsents)
 
-    def get_brat(self):
+    def get_brat(self, batch_files):
+        (self._root_dir / 'brat').mkdir(parents=True, exist_ok=True)
         
-        args = Namespace(**self.ner_model_params)
+        args = Namespace(**self.ner_model_params['params'])
         
         args.device                 = self.device
-        args.batch_files            = list(self.bio_init.keys())
-        args.sents                  = copy.deepcopy(self.bio_init)
+        args.batch_files            = batch_files
         args.preprocessed_text_dir  = self._root_dir / 'bio_init'
+        args.progress_bar           = False     # This field is required
         args.logger                 = TransformerNERLogger(self._root_dir / 'logs' / f"ner_{self.gpu_idx}.log", 'i').get_logger()
 
-        labeled_bio = run_ner(args, return_labeled_bio=True)
+        labeled_bio = run_ner(args, return_labeled_bio=True, sents=self.bio_init, raw_text=self.encoded_text) # TODO: don't use deepcopy if not necessary
         
-        self.brat = format_converter(text_dir=str(self._root_dir / 'encoded_text'),
-                                    input_bio_dir=str(self._root_dir / 'encoded_text'),
-                                    output_dir=str(self._root_dir / 'brat'),
-                                    formatter=1, do_copy_text=False, labeled_bio_tup_lst=labeled_bio, use_bio=False, return_dict=True)
+        format_converter(str(self._root_dir / 'encoded_text'),
+                        str(self._root_dir / 'encoded_text'),
+                        str(self._root_dir / 'brat'),
+                        "{}\t{} {} {}\t{}", False, labeled_bio_tup_lst=labeled_bio, write_output=True, use_bio=False, return_dict=self.brat)
+
+    def get_result(self, result, batch_files):
         
-    def get_output(self, output, batch_files):
-        
-        _exist = self.load_output(output, batch_files) 
+        _exist = self.load_result(result, batch_files) 
         if not _exist:
             
-            for dependency in self.dependency_tree[output]:
-                self.get_output(dependency, batch_files)
+            for dependency in self.dependency_tree[result]:
+                self.get_result(dependency, batch_files)
 
-            funct = getattr(self, f"get_{output}")
+            funct = getattr(self, f"get_{result}")
             funct(batch_files)            
         return 
     
     def run(self):
     
-        for subdir in self.get_subdirs(self.root_dir):
-            gap = relpath(str(subdir), self.root_dir)
+        for subdir in self.get_subdirs(self.raw_data_dir):
+            gap = relpath(str(subdir), self.raw_data_dir)
             self._root_dir = self.root_dir / gap
             
             for batch_files in self.get_batch_files(subdir):
                 
-                self.get_output(self.output, batch_files)
+                self.get_result(self.result, batch_files)
                 self.clear_cache()
-
-class PipelineManager(object):
-
-    def __init__(self, device=None, root_dir=None, raw_data_dir=None, ner_model={}, relation_model={}, negation_model={}, gpu_nodes=(0, 1), 
-                 sent_tokenizer={}, batch_sz=1e4, dependency_tree=[], result=None):
-        
-        self.device             = device
-        self.root_dir           = Path(root_dir) if root_dir else None
-        self.raw_data_dir       = Path(raw_data_dir) if raw_data_dir else None
-        self.ner_model          = ner_model
-        self.relation_model     = relation_model
-        self.negation_model     = negation_model
-        self.sent_tokenizer     = sent_tokenizer
-        self.gpu_nodes          = gpu_nodes
-        self.batch_sz           = batch_sz
-        
-        self.result             = result
-        
-        # Dependency tree as parent-children dictionary
-        self.dependency_tree    = defaultdict(list)
-        for dependency in dependency_tree:
-            self.dependency_tree[dependency[0]].append(dependency[1])
-        
-    # root_dir: /data/datasets/shared_data_2/IRB201901754_lungrads
-    # raw_data_dir: /data/datasets/shared_data_2/IRB201901754_lungrads/raw_text/
-    # ner_model:
-    # type: roberta
-    # path: /home/jameshuang/Projects/NLP_annotation/fine_tuned_ner/mimiciii_roberta-base_10e_128b_bz=8
-    # negation_model:
-    # type: bert-base
-
-    def run(self):
-        pass
-
 
 def main(experiment_info):
     
@@ -250,12 +246,11 @@ if __name__ == "__main__":
     parser.add_argument("--config", type=str, required=True, help="configuration file")
     parser.add_argument("--experiment", type=str, required=True, help="experiement to run")
     parser.add_argument("--gpu_nodes", nargs="+", default=[0], help="gpu_device_id")
-    parser.add_argument("--result", type=str, default='output', help="result to generate")
-    # sys_args = ["--config", "/home/jameshuang/Projects/NLP_annotation/params/config.yml", "--experiment", "lungrads_ner_validation_baseline"]
-    # sys_args = ["--config", "/home/jameshuang/Projects/NLP_annotation/params/config.yml", "--experiment", "lungrads_pipeline", "--gpu_nodes", "0", "1", "2", "3"]
-    # sys_args = ["--config", "/home/jameshuang/Projects/NLP_annotation/params/config.yml", "--experiment", "SDoH_pipeline", "--gpu_nodes", "0", "1", "2", "3", "4"]
-    # args = parser.parse_args(sys_args)
-    args = parser.parse_args()
+    parser.add_argument("--result", type=str, default='output_csv', choices=OUTPUT_DIR, help="result to generate")
+
+    sys_args = ["--config", "/home/jameshuang/Projects/pipeline_dev/pipeline_config.yml", "--experiment", "lungrads_pipeline", "--result", "brat", "--batch_sz", "501"]
+    args = parser.parse_args(sys_args)
+    # args = parser.parse_args()
     
     # Load configuration
     with open(Path(args.config), 'r') as f:
